ef8c97157aa0e1b80ec30757dfee007df5e6fcd9
francisobed <francisobadiahajigana@gmail.com>
2020-08-16 09:14:31 +0100

    add some styling to the input-form and also some little update in the sign-in component

--- src/Components/sign-in/Sign-in.js
+++ src/Components/sign-in/Sign-in.js
@@ -1,4 +1,6 @@
 import React from 'react'
+
+import FormInput from '../form-input/form-input'
 import './Sign-in.scss'
 
 class SignIn extends React.Component {
@@ -29,23 +31,23 @@ class SignIn extends React.Component {
                 <span>Sign in with your email and password</span>
 
                 <form onSubmit={this.handleSubmit}>
-                    <input 
+                    <FormInput 
                         name='email' 
                         type='email' 
                         value={this.state.email} 
                         onChange={this.handleChange}
+                        label='password'
                         required 
                     />
-                    <label>Email</label>
 
-                    <input 
+                    <FormInput 
                         name='password' 
                         type='password' 
                         value={this.state.password} 
-                        onChange={this.handleChange}
+                        handleChange={this.handleChange}
+                        label='email'
                         required 
                      />
-                    <label>Password</label>
 
                     <input type='submit' value='Submit Form' />
                 </form>
