7f173b519ac8736dedc4068e737abc5c45fb81ee
obadiahf <obadiahf@accessbankplc.com>
2021-05-13 10:23:57 +0100

    added cartIcon and CartDropdown components to the application, also implemented card reducer

--- src/Components/cart-icon/cart-icon.js
+++ src/Components/cart-icon/cart-icon.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+import { toggleCartHidden } from '../../assets/11.2 shopping-bag.svg.svg'
 
 import { ReactComponent as ShoppingIcon } from '../../assets/11.2 shopping-bag.svg.svg';
 
@@ -6,9 +9,15 @@ import './cart-icon.scss'
 
 const CartIcon = () => (
     <div className='cart-icon'>
-    <ShoppingIcon className='shopping-icon'/>
-    <span className='item-count'>0</span>
+        <ShoppingIcon className='shopping-icon'/>
+        <span className='item-count'>0</span>
     </div>
     );
 
-    export default CartIcon;
+    const mapDispatchToProps = dispatch => ({
+        toggleCartHidden: () => dispatch(toggleCartHidden())
+    });
+    export default connect(
+        null,
+        mapDispatchToProps
+    ) (CartIcon);
